a15dac85641adef3dc751df10ed858378e45a9b9
Ruggy Mango <rans2203@gmail.com>
2024-05-29 22:00:02 +0530

    Add starters 136 problems

---
+++ Starters136/WhoMakesP1.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/START136D/problems/WHOMAKESP1?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if (a>b) {
+	    cout<<"Dom";
+	}
+	else {
+	    cout<< "Tyro";
+	}
+}
