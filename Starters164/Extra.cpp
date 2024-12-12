102377d5c8f3b1767e3fc0f6e8e2b71288ef22fe
rajanraj2 <rajanraj253545@gmail.com>
2024-12-12 21:03:16 +0530

    Solved starters 164 div 3 problems

---
+++ Starters164/Extra.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/SPC2025
+// New-pro coder
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n, m;
+	cin >> n >> m;
+	if (m >= (n + 1) / 2) cout << "NEWBIE" << endl;
+	else cout << "PRO" << endl;
+}
