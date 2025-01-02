e9135f9a48465b09f3dd3191cdc36524e16584f3
zorax <rajanraj253545@gmail.com>
2025-01-02 18:58:11 +0530

    Solved starters 167 div 3 problems

--- Starters167/D.cpp
+++ Starters167/D.cpp
@@ -12,106 +12,20 @@ int main() {
 	while (t--) {
 	    int n;
 	    cin >> n;
-	    vector<int> arr(n);
+	    vector<long long> arr(n);
 	    for (int i = 0; i < n; i++) {
 	        cin >> arr[i];
 	    }
-	    if (n <= 1) return 0;
 	    
-	    int result = 0;
+	    long long result = 0;
+	    long long pre = 0;
 	    for (int i = 0; i < n - 1; i++) {
-	        if (arr[i] != 0) {
-	            result += abs(arr[i]);
-	            arr[i + 1] += arr[i];
-	            arr[i] = 0;
-	        }
+	        result += (long long)abs(arr[i]);
+	        arr[i + 1] += arr[i];
+	       // pre += arr[i]; // till i < n
+	       // result += abs(pre);
 	    }
-	    
 	    cout << result << endl;
 	}
 
 }
-
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-//   // your code goes here
-//   int t;
-//   cin >> t;
-//   while (t--) {
-//       int n;
-//       cin >> n;
-//       vector<int> arr(n);
-//       bool flag = true;
-//       for (int i = 0; i < n; i++) {
-//           cin >> arr[i];
-//           if (flag && arr[i] == 1) flag = false;
-//       }
-//       int chef = arr[0];
-//      // sort(arr.begin(), arr.end());
-//       int mini = 1, maxi = 1e6;
-//       for (int i = 0; i < n; i++) {
-//           if (arr[i] == chef) {
-//               if (i > 0) mini = arr[i - 1];
-//               if (i < n - 1) maxi = arr[i + 1];
-//               break;
-//           }
-//       }
-      
-//       int result = 1;
-//       if (n == 1) {
-//           cout << 1000000 << endl;
-//           continue;
-//       }
-      
-//       for (int it = 1; it <= 1e6; it++) {
-//           bool check = true;
-//           int diff = abs(chef - it);
-          
-//           for (int i = 1; i < n; i++) {
-//               int d = abs(arr[i] )
-//           }
-//       }
-
-      
-      
-      
-//      // int maxi = 1e6, mini = 1;
-//      // bool flag = false;
-//      // for (int i = 1; i < n; i++) {
-//      //     if (arr[i] == 1) flag = true;
-//      //     if (arr[i] > chef) maxi = min(maxi, arr[i]);
-//      //     if (arr[i] < chef) mini = max(mini, arr[i]);
-//      // }
-//      // int result = 1;
-//       if (maxi == chef + 1) {
-//           result += 0;
-//       }
-//       else if (maxi == 1e6) {
-//           result += maxi - chef;
-//          // cout << result << "@" << endl;
-//       }
-//       else {
-//           result += (maxi - chef - 1) / 2;
-//           if ((maxi - chef - 1) % 2 == 1) result++;
-//          // cout << result << "@" << endl;
-//       }
-      
-//       if (mini == chef - 1) {
-//           result += 0;    
-//       }
-//       else if (mini == 1 && flag) {
-//           result += chef - mini;
-//          // cout << "flag" << endl;
-//       }
-//       else {
-//           result += (chef - mini - 1) / 2;
-//           if ((chef - mini - 1) % 2 == 1) result++;
-//          // cout << result << "#" << endl;
-//       }
-//      // if (maxi == (chef + 1) && mini == (chef - 1)) result = 1;
-//       cout << result << endl;
-//   }
-// }
