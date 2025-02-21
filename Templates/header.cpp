dd506702452b6bb49989b1dce4da32a16260ba77
zorax <rajanraj253545@gmail.com>
2025-02-21 07:59:37 +0530

    Add templates

---
+++ Templates/header.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define int long long int 
+#define vi vector<int>
+#define w(x) int x; cin>>x; while(x--)
+#define pb push_back
